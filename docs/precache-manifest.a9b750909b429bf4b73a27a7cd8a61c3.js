081f6f072dc5549216cb4480b19e9bf5589300c6
Jussi Karppinen <jkarppin@Jussi-MacBook-Pro.local>
2018-10-08 15:08:47 +0300

    updated homepage info

---
+++ docs/precache-manifest.a9b750909b429bf4b73a27a7cd8a61c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e7ab1e1ce6fc279ebda",
+    "url": "/timetable-fe/docs/static/js/runtime~main.5e7ab1e1.js"
+  },
+  {
+    "revision": "1036f25bba86d2a5a2d1",
+    "url": "/timetable-fe/docs/static/js/main.1036f25b.chunk.js"
+  },
+  {
+    "revision": "3aa7781b752414659292",
+    "url": "/timetable-fe/docs/static/js/1.3aa7781b.chunk.js"
+  },
+  {
+    "revision": "1036f25bba86d2a5a2d1",
+    "url": "/timetable-fe/docs/static/css/main.b4ca42ae.chunk.css"
+  },
+  {
+    "revision": "996ad0643faa04eb0aa852954f68f29d",
+    "url": "/timetable-fe/docs/index.html"
+  }
+];
