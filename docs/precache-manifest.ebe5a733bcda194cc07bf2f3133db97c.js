ff22de3511a441209d5e8079a75db8aba5fcc53f
Jussi Karppinen <jkarppin@Jussi-MacBook-Pro.local>
2018-10-08 15:01:28 +0300

    initial commit

---
+++ docs/precache-manifest.ebe5a733bcda194cc07bf2f3133db97c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "878567a89af2b66633c9",
+    "url": "/static/js/main.878567a8.chunk.js"
+  },
+  {
+    "revision": "f2eb4fc4cd215d14e149",
+    "url": "/static/js/1.f2eb4fc4.chunk.js"
+  },
+  {
+    "revision": "878567a89af2b66633c9",
+    "url": "/static/css/main.b4ca42ae.chunk.css"
+  },
+  {
+    "revision": "a550494dcf714014b0d1788efcd03320",
+    "url": "/index.html"
+  }
+];
