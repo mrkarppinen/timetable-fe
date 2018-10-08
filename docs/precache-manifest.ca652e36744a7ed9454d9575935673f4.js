37c94f029f2a5dc91b1789cb7a8d49911b60e144
Jussi Karppinen <jkarppin@Jussi-MacBook-Pro.local>
2018-10-08 15:34:25 +0300

    Router fix

---
+++ docs/precache-manifest.ca652e36744a7ed9454d9575935673f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e7ab1e1ce6fc279ebda",
+    "url": "/timetable-fe/docs/static/js/runtime~main.5e7ab1e1.js"
+  },
+  {
+    "revision": "36c66b1427dd3af05e92",
+    "url": "/timetable-fe/docs/static/js/main.36c66b14.chunk.js"
+  },
+  {
+    "revision": "9aa73db487c97ec6cfed",
+    "url": "/timetable-fe/docs/static/js/1.9aa73db4.chunk.js"
+  },
+  {
+    "revision": "36c66b1427dd3af05e92",
+    "url": "/timetable-fe/docs/static/css/main.1569018e.chunk.css"
+  },
+  {
+    "revision": "28b87e02d43943486301c3c5937ad557",
+    "url": "/timetable-fe/docs/index.html"
+  }
+];
